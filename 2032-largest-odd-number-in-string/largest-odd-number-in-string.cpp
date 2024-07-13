5747ca966bc1ed8d30bc9c49f131b183eb7304bd
Shivani Singh <99162040+shivani19003@users.noreply.github.com>
2024-07-13 13:49:56 +0530

    Time: 28 ms (56.93%) | Memory: 16.6 MB (17.77%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string largestOddNumber(string num) {
         string gg="";
         for(int i=num.size()-1; i>=0; i--){
-            int r=num[i]-'0';
+            int r=num[i]-48;
             if(r%2!=0){
                 gg=num.substr(0,i+1);
                 break;
