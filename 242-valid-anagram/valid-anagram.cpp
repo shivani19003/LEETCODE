1a85ecf0b30ae43bc597893f84a2d71ed12776b1
Shivani Singh <99162040+shivani19003@users.noreply.github.com>
2024-07-13 21:57:54 +0530

    Time: 8 ms (63.92%) | Memory: 8.8 MB (41.78%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char, int> mp;
-        for(int i=0; i<t.size(); i++){
-            mp[t[i]]++;
+        sort(s.begin(), s.end());
+        sort(t.begin(),t.end());
+        if(s==t){
+            return true;
         }
-        for(int i=0; i<s.size(); i++){
-            if(mp[s[i]]){
-                mp[s[i]]--;
-            }
-            else{
-                return false;
-            }
-        }
-        for(auto i : mp){
-            if(i.second>=1){
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 };
