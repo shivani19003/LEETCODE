a90fccecc1166a13a677c0233ab2154b3636e7e1
Shivani Singh <99162040+shivani19003@users.noreply.github.com>
2024-07-19 01:37:34 +0530

    Time: 7 ms (88.80%) | Memory: 10.9 MB (57.40%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        map<char, int> mp;
-        for(int i=0; i<s.length(); i++){
-            mp[s[i]]++;
-        }
         string gg="";
+        map<char, int> mp;
+        for(auto i: s){
+            mp[i]++;
+        }
         while(!mp.empty()){
         int maxi=INT_MIN;
-        for(auto i : mp){
-            maxi=max(maxi, i.second);
-        }
-        for(auto i: mp){
-            if(i.second==maxi){
-                while(i.second--){
-                    gg+=i.first;
-                }
-                mp.erase(i.first);
+            for(auto i : mp){
+                maxi=max(maxi, i.second);
             }
-        }
+            for(auto i : mp){
+                if(i.second == maxi){
+                    while(i.second--){
+                        gg+=i.first;
+                    }
+                    mp.erase(i.first);
+                }
+            }
         }
         return gg;
     }
