ede2c12b28429b05f88d1e2359e2e055956feed5
Shivani Singh <99162040+shivani19003@users.noreply.github.com>
2024-09-19 18:35:16 +0530

    Time: 142 ms (56.23%) | Memory: 137.1 MB (36.75%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> posi;
-        vector<int>neg;
-        for(int i=0 ; i<nums.size(); i++){
+        vector<int> p;
+        vector<int> n;
+        for(int i=0; i<nums.size(); i++){
             if(nums[i]>0){
-                posi.push_back(nums[i]);
+                p.push_back(nums[i]);
+            }
+            else n.push_back(nums[i]);
+        }
+        int d=0;
+        int e=0;
+        for(int i=0; i<nums.size(); i++){
+            if(i%2==0){
+                nums[i]=p[d];
+                d++;
             }
             else{
-                neg.push_back(nums[i]);
+                nums[i]=n[e];
+                e++;
             }
         }
-        vector<int> ans;
-        int i=0; 
-        int j=0;
-        int num=0;
-        while(num<nums.size()){
-            if(num%2==0){
-                ans.push_back(posi[i]);
-                i++;
-            }
-            else{
-                ans.push_back(neg[j]);
-                j++;
-            }
-            num++;
-        }
-        return ans;
+        return nums;
     }
 };
-
-// [3, 1, 2]
-// [-2, -2, -4]
